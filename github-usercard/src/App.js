64b04d4e58b4a37fba1567ad1291c9c66a52e1bf
Jorge Jimenez <calenocam24@gmail.com>
2020-08-08 16:22:30 -0400

    finished styling and MVP

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -2,14 +2,44 @@ import React from "react";
 import UserGitHubCard from "./components/UserGitHubCard";
 import FollowersCard from "./components/FollowersCard";
 import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    padding-bottom: 40px;
+    text-shadow: 3px 3px 2px gray;
+    font-size: 40px;
+  }
+
+  button {
+    border: 1px solid black;
+    border-radius: 4px;
+    background-color: black;
+    color: white;
+    padding: 3px 5px 3px 5px;
+    cursor: pointer;
+
+    font-size: 18px;
+    &: hover {
+      color: #fff8dc;
+    }
+  }
+`;
 
 class App extends React.Component {
   constructor() {
     console.log("constructor is invoked");
     super();
     this.state = {
-      userGitHub: [],
-      followers: []
+      user: [],
+      followers: [],
     };
   }
 
@@ -18,38 +48,41 @@ class App extends React.Component {
     axios
       .get("https://api.github.com/users/Caleno83")
       .then((res) => {
-        this.setState({ userGitHub: res.data });
-        console.log(" This is the user response:", this.state.userGitHub);
+        this.setState({ user: res.data });
+        console.log(" This is the user response:", this.state.user);
       })
       .catch((err) => console.log("This is the Error:", err));
+  }
 
-      axios
+  handleFollowers = (e) => {
+    console.log("this is my handleChange invoked");
+    e.preventDefault();
+    axios
       .get("https://api.github.com/users/Caleno83/followers")
       .then((res) => {
         this.setState({ followers: res.data });
         console.log(" This is the followers response:", this.state.followers);
       })
       .catch((err) => console.log("This is the followers Error:", err));
-  
-  }
+  };
+
 
   componentDidUpdate(prevState, prevProps) {
-    console.log("CDU is invoked")
-    if (prevState.UserGitHub !== this.state.userGitHub) {
+    console.log("CDU is invoked");
+    if (prevState.user !== this.state.userGitHub) {
       console.log("User GitHub have changed!");
     }
   }
 
- 
-
   render() {
     console.log("render is invoked");
     return (
-      <div className="App">
+      <Container>
         <h1>My GitHub Profile</h1>
-        <UserGitHubCard users={this.state.userGitHub} />
+        <UserGitHubCard users={this.state.user} />
+        <button onClick={this.handleFollowers} >Click For Followers</button>
         <FollowersCard followers={this.state.followers} />
-      </div>
+      </Container>
     );
   }
 }
--- github-usercard/src/components/FollowersCard.js
+++ github-usercard/src/components/FollowersCard.js
@@ -1,18 +1,64 @@
 import React from "react";
+import styled from "styled-components";
+
+const FollowersContainer = styled.div`
+  width: 100%;
+  padding: 30px;
+
+  border-radius: 5px;
+  box-shadow: 0 1px 6px -2px #000;
+  background-color: #5f9ea0;
+  margin: 15px;
+
+  .right {
+    float: right;
+    margin: 30px 100px 0 0;
+  }
+
+  img {
+    width: 250px;
+    height: 250px;
+    border-radius: 3px;
+    margin-left: 60px;
+  }
+
+  .username {
+    font-size: 18px;
+    font-style: italic;
+    margin: 3px 0 10px;
+  }
+
+  .link {
+    a {
+      text-decoration: none;
+      color: black;
+      &: hover {
+        color: silver;
+      }
+    }
+  }
+`;
 
 const FollowersCard = (props) => {
   console.log(" FollowersCard component is invoked");
 
   return (
     <>
-        {props.followers.map(users => (
-            <div key={users.id} >
-      <img src={users.avatar_url} alt={users.name} />
-      <h2>{users.name}</h2>
-      <h3>{users.login}</h3>
-      <p>{users.html_url}</p>
-      </div>
-        ))}
+      {props.followers.map((followers) => (
+        <FollowersContainer>
+          <div key={followers.id}>
+            <img src={followers.avatar_url} alt={followers.name} />
+            <div className="right">
+              <div className="username">
+                <h3>Username: {followers.login}</h3>
+              </div>
+              <div className="link">
+                <a href={followers.html_url}>Website: {followers.html_url}</a>
+              </div>
+            </div>
+          </div>
+        </FollowersContainer>
+      ))}
     </>
   );
 };
